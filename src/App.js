3e2d830c241b759f4bdd349ccc4217e2a4c2dfa5
Sage <tuzzy08@gmail.com>
2019-05-07 01:15:55 +0100

    Added Placeholder components for Header & Menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 // import './App.css';
 import './assets/css/base.css'
 import Preloader from './components/Preloader/Preloader'
+import Header from './containers/Header/Header'
+import Menu from './containers/Header/Menu/Menu'
 
 function App() {
   return (
     <div className="App">
       <Preloader id="preloader" />
+      <Header>
+        <Menu />
+      </Header>
     </div>
   );
 }
---
+++ src/containers/Header/Header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Header = (props) => props.children
+
+export default Header
